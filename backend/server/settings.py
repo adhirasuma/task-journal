f6b2538c74925420a757985944ace18d3cbad1dc
adhirasuma <adhirasuma@gmail.com>
2026-04-29 09:20:18 +0530

    Updated API URL to production backend

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -28,10 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CORS_ALLOWED_ORIGINS=[
-    "http://localhost:3000",
-    "http://127.0.0.1:8000",
-]
+CORS_ALLOWED_ORIGINS=True
 
 # Application definition
 
--- frontend/src/Components/axiosInstance.jsx
+++ frontend/src/Components/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:process.env.REACT_APP_API_URL,
+  baseURL:'https://todolist-6be2.onrender.com/',
 });
 
 // request interceptor
